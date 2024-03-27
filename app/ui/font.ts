561f09721b995409a3d808f73ce994ee17686570
syusakusai <bapamu55@gmail.com>
2024-03-27 13:39:04 +0700

    init commit

---
+++ app/ui/font.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lustiana = Lusitana({ weight: ['700'], preload:false,  subsets: ['latin'], });
